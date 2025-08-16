350f73e3c958c95cf6230abb6a6320270f058b44
fsangregorio <franciscosangregorio@hotmail.com>
2025-08-15 22:49:51 -0400

    lint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,11 +1,12 @@
-
-from django.contrib.auth.models import User
-from .models import CarMake, CarModel
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
 import logging
 import json
+
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.contrib.auth import login, authenticate
 from django.views.decorators.csrf import csrf_exempt
+
+from .models import CarMake, CarModel
 from .populate import initiate
 from .restapis import get_request, analyze_review_sentiments, post_review
 
@@ -15,23 +16,25 @@ logger = logging.getLogger(__name__)
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if count == 0:
         initiate()
-    car_models = CarModel.objects.select_related('car_make')
+    car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name,
-        "CarMake": car_model.car_make.name})
+        cars.append(
+            {
+                "CarModel": car_model.name,
+                "CarMake": car_model.car_make.name,
+            }
+        )
     return JsonResponse({"CarModels": cars})
 
+
 @csrf_exempt
-
-
-
 def login_user(request):
     data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
+    username = data["userName"]
+    password = data["password"]
     user = authenticate(username=username, password=password)
     data = {"userName": username}
     if user is not None:
@@ -39,38 +42,40 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
-@csrf_exempt
-
-
-
-def logout_user(request): data = {"userName": ""}
-return JsonResponse(data)
 
 @csrf_exempt
+def logout_user(request):
+    data = {"userName": ""}
+    return JsonResponse(data)
 
 
-
-def registration(request):    
+@csrf_exempt
+def registration(request):
     data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
+    username = data["userName"]
+    password = data["password"]
+    first_name = data["firstName"]
+    last_name = data["lastName"]
+    email = data["email"]
     username_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except Exception:
         # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
+        logger.debug("%s is new user", username)
 
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username,
-        first_name=first_name, last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email,
+        )
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
@@ -80,32 +85,31 @@ def registration(request):
 
 
 def get_dealerships(request, state="All"):
-    if (state == "All"):
+    if state == "All":
         endpoint = "/fetchDealers"
     else:
-        endpoint = "/fetchDealers/"+state
+        endpoint = "/fetchDealers/" + state
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
 
 
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
-    if (dealer_id):
-        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+    if dealer_id:
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
+            response = analyze_review_sentiments(review_detail["review"])
             print(response)
-            review_detail['sentiment'] = response['sentiment']
+            review_detail["sentiment"] = response["sentiment"]
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
 
-
 def get_dealer_details(request, dealer_id):
-    if (dealer_id):
-        endpoint = "/fetchDealer/"+str(dealer_id)
+    if dealer_id:
+        endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
     else:
@@ -113,16 +117,18 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous is False):
+    if request.user.is_anonymous is False:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401,
-                                 "message": "Error in posting review"})
+        except Exception:
+            return JsonResponse(
+                {"status": 401, "message": "Error in posting review"}
+            )
         finally:
             print("add_review request successful!")
     else:
-        return JsonResponse({"status": 403,
-                             "message": "Unauthorized"})
+        return JsonResponse(
+            {"status": 403, "message": "Unauthorized"}
+        )
